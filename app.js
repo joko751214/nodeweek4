4809d8a1dacfef94c0f3c86dbc54066e6cd9ba1c
joko751214 <joko751214@gmail.com>
2022-05-17 08:39:52 +0800

    add cors package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const { errorHandle } = require("./service/handler");
-
+const cors = require("cors");
 const postRouter = require("./routes/posts");
 const userRouter = require("./routes/users");
 
@@ -11,23 +11,12 @@ const app = express();
 
 require("./connections");
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors());
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
 
