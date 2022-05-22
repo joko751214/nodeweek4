64c488d1c8b75545750bba94acb63390fc1925f0
joko751214 <joko751214@gmail.com>
2022-05-22 14:52:08 +0800

    添加其他的 controller 函式

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,15 +20,47 @@ const post = {
   },
 
   createPost: async (req, res, next) => {
-    let image = "";
     const { user, content } = req.body;
     if (!user) return appError(400, "缺少 user ID", next);
     if (!content) return appError(400, "未填寫貼文內容", next);
+    let image = "";
     if (req.file) {
       const { data } = await ImageController.uploadOneFile(req);
       image = data.link !== undefined ? data.link : "";
     }
     const data = await Post.create({
+      image,
+      user,
+      content,
+    });
+    handleSuccess(res, data);
+  },
+
+  deleteAll: async (req, res) => {
+    await Post.deleteMany({});
+    handleSuccess(res, []);
+  },
+
+  deletePost: async (req, res, next) => {
+    const id = req.params.id;
+    const data = await Post.findByIdAndDelete(id);
+    if (!data) return appError(400, "貼文 ID 有誤", next);
+    handleSuccess(res, data);
+  },
+
+  updatePost: async (req, res, next) => {
+    const { user, content } = req.body;
+    if (!user) return appError(400, "缺少 user ID", next);
+    if (!content) return appError(400, "未填寫貼文內容", next);
+
+    let image = "";
+    if (req.file) {
+      const { data } = await ImageController.uploadOneFile(req);
+      image = data.link !== undefined ? data.link : "";
+    }
+
+    const id = req.params.id;
+    const data = await Post.findByIdAndUpdate(id, {
       image,
       user,
       content,
